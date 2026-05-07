ce067d91577f9d4e75a995dfe9055f98adfc9284
cafepromenade <153756308+cafepromenade@users.noreply.github.com>
2026-05-07 16:25:05 -0400

    Automate release creation and JAR publishing on tags

--- src/main/java/game/data/region/McaFile.java
+++ src/main/java/game/data/region/McaFile.java
@@ -186,9 +186,10 @@ public class McaFile {
      * For details on the MCA file format: https://minecraft.gamepedia.com/Region_file_format
      */
     private HashMap<Integer, ChunkBinary> readFile(File mca) throws IOException {
-        FileInputStream inputStream = new FileInputStream(mca);
-        byte[] bytes = IOUtils.toByteArray(inputStream);
-        inputStream.close();
+        byte[] bytes;
+        try (InputStream inputStream = new FileInputStream(mca)) {
+            bytes = IOUtils.toByteArray(inputStream);
+        }
 
         // ensure that the data is not empty
         if (bytes.length == 0) {
